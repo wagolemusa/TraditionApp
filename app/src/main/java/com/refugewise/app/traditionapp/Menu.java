106ef96d25e4915edb1cef1e6db8b475fe36d97f
kisumutalent <homiemusa@gmail.com>
2018-04-27 12:34:35 +0300

    add button and modified

--- app/src/main/java/com/refugewise/app/traditionapp/Menu.java
+++ app/src/main/java/com/refugewise/app/traditionapp/Menu.java
@@ -8,17 +8,41 @@ import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
 
-    private Button b;
+    private Button b1,b2,b3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        b = (Button)findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
+        b1 = (Button) findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Menu.this, LevelOne.class);
+                startActivity(i);
+
+            }
+
+
+        });
+
+        b2 = (Button) findViewById(R.id.button3);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Menu.this, LevelTwo.class);
+                startActivity(i);
+
+            }
+
+
+        });
+
+        b3 = (Button) findViewById(R.id.button4);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Menu.this, LevelThree.class);
                 startActivity(i);
 
             }
